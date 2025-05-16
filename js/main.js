8a2f5bf15cbb7d283616f839aa5c322b4256418b
anouther <155570107+anouther@users.noreply.github.com>
2025-05-16 22:49:29 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Do you remember what day is today?').split('');
+        const titles = ('isaac i love you').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Another month, another chapter of us. Not always perfect, not always easy but always real. Thank you for growing with me, laughing with me, and choosing me, even on the days i forget how to love myself. You're not just my person but you're my peace. Thank you for always being by my side and always being the home i come home to. You're mean so much for me, i'm better with you kaak. i know you love brown so i bought this flower for youuu =33 ";
+            subtitle.innerText = "terimakasi udaaah kasi gift yang spesial juga buat aku, dan terimakasi sudah ngehargain semua gift dari aku";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
